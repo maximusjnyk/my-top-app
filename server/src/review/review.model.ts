c86b93ada50f1033ac914cb0b644423555ee39bc
Maximus <badman7755995@gmail.com>
2022-11-05 16:36:42 +0300

    backend: connected model

--- server/src/review/review.model.ts
+++ server/src/review/review.model.ts
@@ -1,8 +1,24 @@
-export class ReviewModel {
-	_id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+
+export interface ReviewModel extends Base {
+
+}
+export class ReviewModel extends TimeStamps {
+
+	@prop()
 	name: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	descriptions: string;
+
+	@prop()
 	rating: number;
+
+	@prop()
 	createdAt: Date;
 }
